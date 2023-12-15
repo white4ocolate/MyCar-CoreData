ec8c0b49077c91b088208010e5c8feec22136779
white4ocolate <morozovyuriy11@gmail.com>
2023-12-15 16:14:33 +0200

    Initial Commit

---
+++ MyCar/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyCar
+//
+//  Created by white4ocolate on 15.12.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
