b9b1f110a603d0146b95e6bb6d4c15aec9628636
white4ocolate <morozovyuriy11@gmail.com>
2023-12-21 17:50:06 +0200

    add elements to Storyboard

--- MyCar/ViewController.swift
+++ MyCar/ViewController.swift
@@ -5,6 +5,25 @@ class ViewController: UIViewController {
     
     var context: NSManagedObjectContext!
 
+    @IBOutlet weak var segmentedControl: UISegmentedControl!
+    @IBOutlet weak var markLabel: UILabel!
+    @IBOutlet weak var modelLabel: UILabel!
+    @IBOutlet weak var carImageView: UIImageView!
+    @IBOutlet weak var lastTimeStartedLabel: UILabel!
+    @IBOutlet weak var numberOfTripsLabel: UILabel!
+    @IBOutlet weak var ratingLabel: UILabel!
+    @IBOutlet weak var myChoiceImageView: UIImageView!
+    
+    
+    @IBAction func segmentedCtrlPressed(_ sender: UISegmentedControl) {
+    }
+    
+    @IBAction func startEnginePressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func rateItPressed(_ sender: UIButton) {
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -12,4 +31,3 @@ class ViewController: UIViewController {
 
 
 }
-
